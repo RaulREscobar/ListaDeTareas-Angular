2be18778fcec2442f401dc48aeb366cbdaea6d55
RaulREscobar <raul.r.escobar@hotmail.com.ar>
2022-09-03 00:15:07 -0300

    modif task-item

--- src/app/components/tasks-item/tasks-item.component.ts
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from 'src/app/Task';
 import { TASKS } from 'src/app/mook-task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
@@ -11,7 +11,8 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 export class TasksItemComponent implements OnInit {
 
   @Input() task: Task = TASKS[0];
-  
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+
   faTimes = faTimes;
 
   constructor() { }
@@ -19,4 +20,7 @@ export class TasksItemComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onDelete(task: Task) {
+    this.onDeleteTask.emit(task);
+  }
 }
